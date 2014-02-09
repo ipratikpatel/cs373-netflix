8212741990e650750708c8316dc76d4eed145ce2
ipratikpatel <ipratikpatel@gmail.com>
2014-02-09 16:08:45 -0600

    calculates RMSE

--- Netflix.py
+++ Netflix.py
@@ -11,6 +11,8 @@
 # netflix_read
 # -------------
 
+import math
+
 def netflix_read (r) :
 	"""
     read one line of the input file,
@@ -42,10 +44,43 @@ def netflix_solve (r, w) :
     r is a reader
     w is a writer
     """
+
+    correct_answers = read_correct_answers()
+    our_answers = {}
+
     for t in netflix_read(r) :
     	if t[1] :
     		movieid = t[0]
     		w.write(str(movieid) + ":" + "\n")
     	else :
     		rating = netflix_predict(movieid, t[0])
+    		our_answers[(movieid, t[0])] = rating
     		w.write(str(rating) + "\n")
+
+    w.write(str(rmse(our_answers, correct_answers)) + "\n")
+
+def read_correct_answers():
+	ratings_path = "../netflix-tests/irvin-probe_ratings.txt"
+	custid_path = "/u/downing/cs/netflix/probe.txt"
+	correct_answers = {}
+	with open(ratings_path) as ratings_f, open(custid_path) as custid_f:
+		for ratings_line in ratings_f.readlines():
+			if is_movieid(ratings_line):
+				movieid = int(ratings_line[:-2])
+				assert custid_f.readline() == ratings_line
+			else:
+				custid = int(custid_f.readline())
+				rating = int(ratings_line)
+				correct_answers[(movieid, custid)] = rating
+	return correct_answers
+
+def is_movieid(line):
+	return ":" in line
+
+def rmse(our_answers, correct_answers):
+	s = 0.0
+	for mc, rating in our_answers.items():
+		print("%s - %s = %s" % (rating, correct_answers[mc], rating - correct_answers[mc]))
+		s += (rating - correct_answers[mc])**2
+	print("s = %s" % s)
+	return math.sqrt(s/len(our_answers))
